e3121cf79df44bec1f7fba1292887fb8d075b928
lmt <1044512563@qq.com>
2019-11-18 10:26:43 +0800

    m

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/views/home.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      name: 'default',
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: {
+        name: 'Home'
+      }
+    },
+    {
+      name: 'Home',
+      path: '/home',
+      component: Home
     }
+
   ]
 })
