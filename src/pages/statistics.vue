1188e24ef49fba6900d7d3db57927938863da8a8
lmt <1044512563@qq.com>
2019-11-20 10:26:18 +0800

    m

---
+++ src/pages/statistics.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="statistics">
+    11111111
+<div class="main" style="width:600px;height:400px;margin-left:25%;margin-top:8%">
+
+</div>
+</div>
+</template>
+<script>
+// 引入 ECharts 主模块
+var echarts = require('echarts/lib/echarts')
+// 引入柱状图
+require('echarts/lib/chart/bar')
+// 引入提示框和标题组件
+require('echarts/lib/component/tooltip')
+require('echarts/lib/component/title')
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    getEcharts () {
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = echarts.init(document.getElementById('main'))
+      // 绘制图表
+      myChart.setOption({
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      })
+    }
+  },
+  mounted () {
+    this.getEcharts()
+  }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,4 +1,3 @@
-
 import app from '@/App.vue'
 import home from '@/pages/home'
 import patientManage from '@/pages/patientManage'
@@ -6,57 +5,66 @@ import patientInfo from '@/pages/patientInfo'
 import remindLog from '@/pages/remindLog'
 import DoneQuesDetail from '@/pages/DoneQuesDetail'
 import noDoQuesDetail from '@/pages/noDoQuesDetail'
-const routes = [
-  {
-    path: '',
-    redirect: '/home'
+import statistics from '@/pages/statistics'
+
+const routes = [{
+  path: '',
+  redirect: '/home'
+}, {
+  path: '',
+  component: app,
+  name: 'app',
+  children: [{
+    path: '/home',
+    name: 'home',
+    component: home,
+    meta: {
+      keepAlive: false
+    }
   }, {
-    path: '',
-    component: app,
-    name: 'app',
-    children: [{
-      path: '/home',
-      name: 'home',
-      component: home,
-      meta: {
-        keepAlive: false
-      }
-    }, {
-      path: '/patientManage',
-      name: 'patientManage',
-      component: patientManage,
-      meta: {
-        keepAlive: false
-      }
-    }, {
-      path: '/patientInfo',
-      name: 'patientInfo',
-      component: patientInfo,
-      meta: {
-        keepAlive: false
-      }
-    }, {
-      path: '/remindLog',
-      name: 'remindLog',
-      component: remindLog,
-      meta: {
-        keepAlive: false
-      }
-    }, {
-      path: '/DoneQuesDetail',
-      name: 'DoneQuesDetail',
-      component: DoneQuesDetail,
-      meta: {
-        keepAlive: false
-      }
-    }, {
-      path: '/noDoQuesDetail',
-      name: 'noDoQuesDetail',
-      component: noDoQuesDetail,
-      meta: {
-        keepAlive: false
-      }
-    }]
-  }
+    path: '/patientManage',
+    name: 'patientManage',
+    component: patientManage,
+    meta: {
+      keepAlive: false
+    }
+  }, {
+    path: '/patientInfo',
+    name: 'patientInfo',
+    component: patientInfo,
+    meta: {
+      keepAlive: false
+    }
+  }, {
+    path: '/remindLog',
+    name: 'remindLog',
+    component: remindLog,
+    meta: {
+      keepAlive: false
+    }
+  }, {
+    path: '/DoneQuesDetail',
+    name: 'DoneQuesDetail',
+    component: DoneQuesDetail,
+    meta: {
+      keepAlive: false
+    }
+  }, {
+    path: '/noDoQuesDetail',
+    name: 'noDoQuesDetail',
+    component: noDoQuesDetail,
+    meta: {
+      keepAlive: false
+    }
+  },
+  {
+    path: '/statistics',
+    name: 'statistics',
+    component: statistics,
+    meta: {
+      keepAlive: false
+    }
+  }]
+}
 ]
 export default routes
