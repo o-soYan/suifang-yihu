6f4208f85cc41469443329319c407cd73ef3f498
yanyumin <17673160295@163.com>
2019-11-21 09:12:55 +0800

    update

--- src/pages/callLog.vue
+++ src/pages/callLog.vue
@@ -3,7 +3,9 @@
     <div class="logContent">
       <div class="item">
         <div class="itemTitle">2019-09-09 电话回访录音</div>
-        <div class="itemIcon"></div>
+        <div class="itemIcon">
+          <van-icon name="play-circle" />
+        </div>
       </div>
     </div>
   </div>
@@ -24,6 +26,13 @@ export default {
     display: flex;
     justify-content: space-between;
     text-align: left;
+    align-items: center;
+    border-bottom: 1px solid #e5e5e5;
+    .itemIcon {
+      .van-icon {
+        font-size: 0.36rem;
+      }
+    }
   }
 }
 </style>
